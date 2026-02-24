dfd6dc0ca463115192c284896d46041658f3f611
Sabbir832Mia <sabbirahmed32mpi@gmail.com>
2026-02-24 06:19:36 +0600

    First commit

---
+++ script.js/scripter.js
@@ -0,0 +1,9 @@
+let interviewList = [];
+let rejectedList = [];
+
+let currentStatus = 'all-filtering-btn';
+
+const totalCount = document.getElementById('total');
+const availableJobs = document.getElementById('availableJobs');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
